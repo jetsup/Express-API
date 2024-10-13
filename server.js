8ca649077ec0991f29d03426fb64e31058edff7a
George Ngigi <ngigegeorge023@gmail.com>
2024-10-13 23:23:39 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// API Routes
+app.use('/api/v1/user', authRoutes);
+app.use('/api/v1/emp/employees', employeeRoutes);
+
+// Handle undefined routes
+app.use((req, res, next) => {
+    console.log("Route: ", req.url);
+  res.status(404).json({
+    status: false,
+    message: 'Route Not Found',
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
